dde4711793781628da6d0dcc10cb95dfa54d7c1a
César Ordaz <cesar.ordazv@gmail.com>
2023-04-13 16:32:52 -0600

    Added copy function and resolve  out disp features

--- js/encrypter.js
+++ js/encrypter.js
@@ -2,6 +2,9 @@
 let dict = { "a": "ai", "e": "enter", "i": "imes", "o": "ober", "u": "ufat" };
 let special = { "á": "a", "é": "e", "í": "i", "ó": "o", "ú": "u", "ü": "u" }
 let textIn;
+let textOut;
+
+document.getElementById("finaloutput").style.display = "none"; // Oculta el cuadro del output
 
 // Función para remplazar cadenas al encriptar o desencriptar
 function allReplace(str, obj) {
@@ -11,32 +14,46 @@ function allReplace(str, obj) {
     return str;
 };
 
-// Función para encriptar
-function encrypt() {
-    let textOut;
-
-    textIn = document.getElementById("textIn").value; // Lectura del texto introducido
-    textIn = textIn.toLowerCase(); // Asegura minúsculas
-    textIn = textIn.replace(/[áéíóúü]/g, l => special[l]); // Remplazo de las vocales acentuadas según el objeto special
-    textOut = textIn.replace(/[aeiou]/g, l => dict[l]); // Remplazo de las vocales según el diccionario
+// Función para el output
+function Output() {
     document.getElementById("textOut").value = textOut; // Envío del texto cifrado al campo de salida
     document.getElementById("textIn").value = ""; // Limpieza del input
+    document.getElementById("initoutput").style.display = "none"; // Muestra el cuadro con el output
+    document.getElementById("finaloutput").style.display = "initial"; // Muestra el cuadro con el output
+    if (textIn === "") {
+        document.getElementById("initoutput").style.display = "initial"; // Muestra el cuadro con el output
+        document.getElementById("finaloutput").style.display = "none"; // Oculta el cuadro con el oputput cuando no hay entrada
+    }
+}
+
+// Función para encriptar
+function encrypt() {
+    textIn = document.getElementById("textIn").value.toLowerCase(); // Lectura del texto introducido en minúsculas
+    textIn = textIn.replace(/[áéíóúü]/g, l => special[l]); // Remplazo de las vocales acentuadas según el objeto special
+    textOut = textIn.replace(/[aeiou]/g, l => dict[l]); // Remplazo de las vocales según el diccionario
+    Output();
 };
 
 let buttonEncrypt = document.getElementById("encrypt"); // Lectura del estado del botón para encriptar
-buttonEncrypt.onclick = encrypt; // Llamada de la función encriptar en el click
+buttonEncrypt.onclick = encrypt; // Llamada de la función encrypt en el click
 
 // Función para desencriptar
 function decrypt() {
-    let textOut;
-
-    textIn = document.getElementById("textIn").value.toLowerCase(); // Lectura del texto intorducido
-    textIn = textIn.toLowerCase(); // Asegura minúsculas
+    textIn = document.getElementById("textIn").value.toLowerCase(); // Lectura del texto intorducido en minúsculas
     textIn = textIn.replace(/[áéíóúü]/g, l => special[l]); // Remplazo de las vocales acentuadas según el objeto special
     textOut = allReplace(textIn, dict); // Llmado de la función que remplaza las palabars por las vocales según el diccionario 
-    document.getElementById("textOut").value = textOut; // Envío del texto descifrado al campo de salida
-    document.getElementById("textIn").value = ""; // Limpieza del input
+    Output();
 };
 
 let buttonDecrypt = document.getElementById("decrypt"); // Lectura del estado del botón para desencriptar
-buttonDecrypt.onclick = decrypt; // Llamada de la función desencriptar en el click
+buttonDecrypt.onclick = decrypt; // Llamada de la función decrypt en el click
+
+// Función para copiar texto
+function copyText() {
+
+    var text = document.getElementById('textOut').value; // Lectura del texto en el output
+    navigator.clipboard.writeText(text); // Copia del texto al portapapeles
+}
+
+let buttonCopy = document.getElementById("copytext"); // Lectura del estado del botón para copiar
+buttonCopy.onclick = copyText; // Llamada de la función para copiar el texto en el output con el click
